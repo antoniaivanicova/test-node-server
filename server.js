6d4343f4d78a06584e9678336b709e197cdaa22a
antoniaivanicova <xivanicova@stuba.sk>
2022-03-12 15:15:39 +0100

    add bcrypt

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const express = require('express')
+const bcrypt = require('bcrypt')
 
 const PORT = process.env.PORT || 8080
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({test: 123})
+    res.json({
+        test: 123,
+    })
 })
 
 app.listen(PORT)
